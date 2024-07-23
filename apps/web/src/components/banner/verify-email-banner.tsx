f5fd536dc355fe5d6a80bb8ca96c3910b85fc0a3
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-07-24 01:56:51 +0200

    style(ui): mobile responsive for email verify banner

--- apps/web/src/components/banner/verify-email-banner.tsx
+++ apps/web/src/components/banner/verify-email-banner.tsx
@@ -14,17 +14,19 @@ export default function VerifyEmailBanner() {
   if (user.isEmailVerified) return null;
 
   return (
-    <div className="fixed bottom-0 px-16 lg:px-32 xl:px-64 2xl:px-96 pb-8 w-full">
-      <Card className="flex items-center justify-between gap-8 p-4 shadow-2xl">
+    <div className="fixed bottom-0 px-4 sm:px-16 lg:px-32 xl:px-64 2xl:px-96 pb-4 sm:pb-8 w-full">
+      <Card className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-8 p-4 shadow-2xl">
         <div className="flex items-center">
-          <ExclamationTriangleIcon className="size-5 mr-2" />
-          <p>
+          <ExclamationTriangleIcon className="hidden sm:inline sm:size-5" />
+          <p className="text-xs text-center sm:text-base sm:text-left">
             You need to verify your email adress before accessing Fuzzie
             features.
           </p>
         </div>
 
-        <Button variant="outline">Send a new link</Button>
+        <Button variant="outline" className="text-xs sm:text-sm">
+          Send a new link
+        </Button>
       </Card>
     </div>
   );
